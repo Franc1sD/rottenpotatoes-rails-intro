6df240edfe2c390c88c7e569bcc60805a05999c4
Francis Deng <zd2278@columbia.edu>
2025-09-29 20:20:57 +0000

    feat: filter feature for part 1

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,14 @@
 class Movie < ActiveRecord::Base
+  def self.all_ratings
+    return self.pluck(:rating).uniq.sort
+  end
+
+  def self.with_ratings(ratings)
+    if ratings.blank?
+      return self.all
+    else
+      return self.where(rating: ratings)
+    end
+  end
+
 end
